3b85bdc94da7b48c185f96f0df22533ab986408a
Amanekomee <catskytree-contact@yahoo.co.jp>
2021-10-30 00:29:45 +0900

    add test

---
+++ SatisfactorySaveEditor/Cheats/TestCheat.cs
@@ -0,0 +1,20 @@
+﻿using SatisfactorySaveEditor.Model;
+using SatisfactorySaveParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SatisfactorySaveEditor.Cheats
+{
+    class TestCheat : ICheat
+    {
+        public string Name => "test";
+
+        public bool Apply(SaveObjectModel rootItem, SatisfactorySave saveGame)
+        {
+            return true;
+        }
+    }
+}
